a6eeff47633ce8af04efd4cb5f367a7df94d24a2
florgironi <gironiflorencia@gmail.com>
2023-02-02 14:31:05 -0300

    Cambie el disenno de estudios, comprimi imagenes y agrege un script para darle funcionalidad a las cosas

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+// ESTUDIOS 
+const despliegueTarjetaEstudio = document.getElementById("descripcion-estudios");
+
+function despliegueTarjeta(){
+    return despliegueTarjetaEstudio.style.display="flex";
+}
