896ffe15cad715d7d6a0dab547939d7aead8d80d
florgironi <gironiflorencia@gmail.com>
2023-02-02 16:52:44 -0300

    cambio de metas

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,12 @@
 // ESTUDIOS 
 const despliegueTarjetaEstudio = document.getElementById("descripcion-estudios");
-
+const btonForm = document.getElementById("bton-form")
+console.log(btonForm)
 function despliegueTarjeta(){
     despliegueTarjetaEstudio.style.display= "flex";
 }
+
+btonForm.addEventListener("click",function(e){
+    e.preventDefault();
+} )
 
